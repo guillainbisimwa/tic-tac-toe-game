3c166ffb1b8baeb4bed5bb23484547b0617bd76a
guillainbisimwa <guillainbisimwa@hotmail.fr>
2020-10-14 19:18:31 +0200

    Return two boleans valid_nbr_from_board

--- library/Messages.rb
+++ library/Messages.rb
@@ -12,11 +12,15 @@ class Message
   end
 
   def valid_symbols?(symb)
-    return true if symb == 'o' || symb == 'x'
+    return true if symb.to_s == 'o' || symb.to_s == 'x'
+
     false
   end
 
-  def valid_nbr_from_board?(nbr, from, to)
-    nbr >= from && nbr <= to
+  # Return two boolean,
+  # The first boolean is true if the nbr is in range from 1 to 9
+  # The second boolean is true if the nbr is not taken (is in the array)
+  def valid_nbr_from_board?(nbr, from, to, array)
+    [nbr >= from && nbr <= to, array.any?(nbr)]
   end
 end
