b9c5c0b77ff7de6149b8b274561836100a600500
guillainbisimwa <guillainbisimwa@hotmail.fr>
2020-10-19 18:36:39 +0200

    Test if the symbol is o or x

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -14,4 +14,21 @@ describe Message do
       expect(m.name_length?(3, 10)).to eql(true)
     end
   end
+
+  context 'valid_symbols?' do
+    it "Test if the symbol is 'o' or 'x'" do
+      m = Message.new
+      expect(m.valid_symbols?('x')).to eql(true)
+    end
+
+    it "Test if the symbol is 'o' or 'x'" do
+      m = Message.new
+      expect(m.valid_symbols?('a')).to eql(false)
+    end
+
+    it "Test if the symbol is 'o' or 'x'" do
+      m = Message.new
+      expect(m.valid_symbols?('o')).to eql(true)
+    end
+  end
 end
