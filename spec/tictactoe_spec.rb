c1d5c55000f1656cc3a3c45f03490b82a6547a02
guillainbisimwa <guillainbisimwa@hotmail.fr>
2020-10-19 18:51:33 +0200

    Compare two names

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -7,44 +7,60 @@ describe Message do
 
   context 'name_length?' do
     it 'Testing the length of a name' do
-      m = Message.new
-      m.info = 'Martin'
-      expect(m.name_length?(3, 10)).to eql(false)
+      message = Message.new
+      message.info = 'Martin'
+      expect(message.name_length?(3, 10)).to eql(false)
     end
 
     it 'Testing the length of a name' do
-      m = Message.new
-      m.info = 'Ma'
-      expect(m.name_length?(3, 10)).to eql(true)
+      message = Message.new
+      message.info = 'Ma'
+      expect(message.name_length?(3, 10)).to eql(true)
     end
   end
 
   context 'valid_symbols?' do
     it "Test if the symbol is 'o' or 'x'" do
-      m = Message.new
-      expect(m.valid_symbols?('x')).to eql(true)
+      message = Message.new
+      expect(message.valid_symbols?('x')).to eql(true)
     end
 
     it "Test if the symbol is 'o' or 'x'" do
-      m = Message.new
-      expect(m.valid_symbols?('a')).to eql(false)
+      message = Message.new
+      expect(message.valid_symbols?('a')).to eql(false)
     end
 
     it "Test if the symbol is 'o' or 'x'" do
-      m = Message.new
-      expect(m.valid_symbols?('o')).to eql(true)
+      message = Message.new
+      expect(message.valid_symbols?('o')).to eql(true)
     end
   end
 
   context 'valid_nbr_from_board' do
     it 'Check if a number is from 1 to 9 and exists in the array' do
-      m = Message.new
-      expect(m.valid_nbr_from_board?(1, 1, 9, arr)).to eql([true, true])
+      message = Message.new
+      expect(message.valid_nbr_from_board?(1, 1, 9, arr)).to eql([true, true])
     end
 
     it 'Check if a number is from 1 to 9 and exists in the array' do
-      m = Message.new
-      expect(m.valid_nbr_from_board?(0, 1, 9, arr)).to eql([false, false])
+      message = Message.new
+      expect(message.valid_nbr_from_board?(0, 1, 9, arr)).to eql([false, false])
+    end
+  end
+
+  context '' do
+    it 'compare_user_entry' do
+      game_logic = GameLogic.new
+      game_logic.p_1 = 'Martin'
+      game_logic.p_2 = 'Kingsley'
+      expect(game_logic.compare_user_entry?).to eql(false)
+    end
+
+    it 'compare_user_entry' do
+      game_logic = GameLogic.new
+      game_logic.p_1 = 'Martin'
+      game_logic.p_2 = 'Martin'
+      expect(game_logic.compare_user_entry?).to eql(true)
     end
   end
 
