917d3f5e973b21c32184a200e14177dc679c3073
guillainbisimwa <guillainbisimwa@hotmail.fr>
2020-10-19 18:45:32 +0200

    heck if a number is from 1 to 9 and exists in the array

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -1,6 +1,10 @@
 require './lib/game_logic'
 
 describe Message do
+  let(:arr) { [1, 2, 3, 4, 6, 7, 8, 9] }
+  let(:arr_playing) { [1, 2, 3, 'x', 6, 7, 'o', 9] }
+  let(:arr_draw) { ['x', 'x', 'o', 'o', 'x', 'o', 'x', 'o'] }
+
   context 'name_length?' do
     it 'Testing the length of a name' do
       m = Message.new
@@ -31,4 +35,17 @@ describe Message do
       expect(m.valid_symbols?('o')).to eql(true)
     end
   end
+
+  context 'valid_nbr_from_board' do
+    it 'Check if a number is from 1 to 9 and exists in the array' do
+      m = Message.new
+      expect(m.valid_nbr_from_board?(1, 1, 9, arr)).to eql([true, true])
+    end
+
+    it 'Check if a number is from 1 to 9 and exists in the array' do
+      m = Message.new
+      expect(m.valid_nbr_from_board?(0, 1, 9, arr)).to eql([false, false])
+    end
+  end
+
 end
