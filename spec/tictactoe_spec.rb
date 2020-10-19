ae34db38c21274f84f02f2f5ac8fd4e150ec8174
guillainbisimwa <guillainbisimwa@hotmail.fr>
2020-10-19 18:32:23 +0200

    Test the length ot a name

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -2,8 +2,16 @@ require './lib/game_logic'
 
 describe Message do
   context 'name_length?' do
-    it '' do
-      
+    it 'Testing the length of a name' do
+      m = Message.new
+      m.info = 'Martin'
+      expect(m.name_length?(3, 10)).to eql(false)
+    end
+
+    it 'Testing the length of a name' do
+      m = Message.new
+      m.info = 'Ma'
+      expect(m.name_length?(3, 10)).to eql(true)
     end
   end
 end
